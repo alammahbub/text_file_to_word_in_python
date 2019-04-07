030c25f8dfca577b0ebbde91f53d79c2f882d531
Mahbub Alam <men918@gmail.com>
2019-04-07 10:40:44 +0600

    variable name updated for better understanding

--- text2list.py
+++ text2list.py
@@ -1,15 +1,15 @@
-k=[]
-with open('text.txt','r',encoding='utf-8') as f:
-    g = f.readlines()
-for line in g:
+list=[]
+with open('text.txt','r',encoding='utf-8') as file:
+    text = file.readlines()
+for line in text:
     line = line.strip('\n')
     line = line.split(' ')
-    k.append(line)
+    list.append(line)
     print(line)
 # converted list
-print(k)
+print(list)
 # word from the list
-for line in k:
+for line in list:
     for word in line:
         print(word)
     print("    ")
